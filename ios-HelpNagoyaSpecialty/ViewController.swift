8ea2e69ef6d3c335f3bc0a2a2bccc7f924814b74
Masahiro Atsumi <raindro.aqua@gmail.com>
2019-03-20 21:21:07 +0900

    はじめてのコミット

---
+++ ios-HelpNagoyaSpecialty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-HelpNagoyaSpecialty
+//
+//  Created by Masahiro Atsumi on 2019/03/20.
+//  Copyright © 2019年 Masahiro Atsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
